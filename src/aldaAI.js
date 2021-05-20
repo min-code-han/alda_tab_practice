9030303e15a56eceb52af92e7061a6f8921743c2
alda <alda@hanmin-aui-MacBookPro.local>
2021-05-20 10:23:03 +0900

    styled-component 설치

--- src/aldaAI.js
+++ src/aldaAI.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Tab from './tab';
 
 const AldaAI = () => {
     return(
+        <>
         <h1>알다AI 메뉴 컴폰너트</h1>
+        <Tab />
+        </>
     )
 };
 
---
+++ src/tab.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Tab = () => {
+    return(
+        <h1>Tab 컴포넌트 자리 입니다</h1>
+    )
+};
+
+export default Tab;
