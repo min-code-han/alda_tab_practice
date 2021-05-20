a1e4851f652c53adfbd2cd7c2623b4070b7e4c80
alda <alda@hanmin-aui-MacBookPro.local>
2021-05-20 18:40:43 +0900

    레이아웃 완료

--- src/content.js
+++ src/content.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import styled from "styled-components";
@@ -16,15 +16,33 @@ const typosStyle = makeStyles((theme) => ({
 }));
 
 const Content = (props) => {
+  const [popUp, setPopUp] = useState(false);
   const classes = typosStyle();
   const { title, sub1, sub2, noticeIco, tab } = props;
+  const hadlePopUp = () => {
+    setPopUp(!popUp);
+    console.log(popUp);
+  };
   return (
     <>
       <TxtWrap>
         <Typography variant='h5' className={classes.title}>
           {title}
           {noticeIco === "true" && (
-            <i className='fas fa-info-circle tab-information' />
+            <i
+              className='fas fa-info-circle tab-information'
+              onClick={hadlePopUp}
+            />
+          )}
+          {popUp && (
+            <PopUp>
+              React는
+              <br /> 상호작용이 많은 UI를 만들 때 생기는 어려움을 줄여줍니다.
+              애플리케이션의 각 상태에 대한 간단한 뷰만 설계하세요.
+              <br /> 그럼 React는 데이터가 변경됨에 따라 적절한 컴포넌트만
+              효율적으로 갱신하고 렌더링합니다. <br /> 선언형 뷰는 코드를 예측
+              가능하고 디버그하기 쉽게 만들어 줍니다.
+            </PopUp>
           )}
         </Typography>
         <Typography variant='h7'>{sub1}</Typography>
@@ -48,6 +66,7 @@ const Content = (props) => {
 export default Content;
 
 const TxtWrap = styled.div`
+  position: relative;
   height: 200px;
 `;
 
@@ -61,4 +80,18 @@ const Notice = styled.p`
     margin-right: 3px;
     line-height: 1;
   }
+`;
+
+const PopUp = styled.div`
+  z-index: 10;
+  position: absolute;
+  top: 80px;
+  left: 0;
+  padding: 20px 40px;
+  width: 100%;
+  background-color: #ff5c20;
+  text-align: center;
+  font-size: 15px;
+  color: #fff;
+  box-sizing: border-box;
 `;
