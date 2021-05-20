45e70d55bcd14f3def6c9965bc0b9d9a15be4092
alda <alda@hanmin-aui-MacBookPro.local>
2021-05-20 10:52:01 +0900

    Tab Component 생성

--- src/aldaAI.js
+++ src/aldaAI.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import TabComponent from './tab';
+import styled from 'styled-components'
+import TabPanel from './tab';
 
 const AldaAI = () => {
     return(
-        <>
-        <h1>알다AI 메뉴 컴폰너트</h1>
-        <TabComponent />
-        </>
+        <Wrap>
+            <h1>알다AI 메뉴 컴폰너트</h1>
+            <TabPanel />
+        </Wrap>
     )
 };
 
 export default AldaAI;
+
+const Wrap = styled.div`
+    width:500px;
+`;
--- src/tab.js
+++ src/tab.js
@@ -1,10 +1,95 @@
 import React from 'react';
-import {AppBar,Tabs,Tab, Typography,Box,PropTypes,SwipeableViews} from '@material-ui/core'
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-const TabComponent = () => {
-    return(
-        <h1>Tab 컴포넌트 자리 입니다</h1>
-    )
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`nav-tabpanel-${index}`}
+      aria-labelledby={`nav-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
 };
 
-export default TabComponent;
+function a11yProps(index) {
+  return {
+    id: `nav-tab-${index}`,
+    'aria-controls': `nav-tabpanel-${index}`,
+  };
+}
+
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function NavTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" >
+        <Tabs
+          variant="fullWidth"
+          value={value}
+          onChange={handleChange}
+          aria-label="nav tabs example"
+        >
+          <LinkTab label="신용 올리기" href="/drafts" {...a11yProps(0)} />
+          <LinkTab label="대출 이자 줄이기" href="/trash" {...a11yProps(1)} />
+          <LinkTab label="맞춤 대출 찾기" href="/spam" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Page One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Page Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Page Three
+      </TabPanel>
+    </div>
+  );
+}
